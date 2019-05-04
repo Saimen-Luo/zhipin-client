69d02c1b39c3611a3ed4476ad3f13fd67469e397
Saimen-Luo <lswofficial@163.com>
2019-05-04 11:55:35 +0800

    encapsulating api function

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+/* 包含了所有的接口请求的函数的模块
+    返回promise 对象
+*/
+import ajax from './ajax'
+
+// 用户注册接口
+export const reqRegister = (user) => ajax('/register', user, "POST")
+
+// 登录接口
+export const reqLogin = ({ username, password }) => ajax('/login', { username, password }, "POST")
+
+// 更新用户信息接口
+export const reqUpdate = (user) => ajax('/update', user, "POST")
