0cec93815d03bfe03f6c2e4c2e074bdb9635b6a0
Saimen-Luo <lswofficial@163.com>
2019-05-04 17:48:27 +0800

    front-end form verify

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -14,8 +14,19 @@ const authSuccess = (user) => ({ type: AUTH_SUCCESS, data: user })
 // 错误提示的同步action
 const errMessage = (msg) => ({ type: ERR_MESSAGE, data: msg })
 
+// 注册异步action
 export const register = (user) => {
     const { username, password, password2, type } = user
+    // 表单前台验证
+    if (!username) {
+        return errMessage('用户名不能为空')
+    }else if (!password) {
+        return errMessage('密码不能为空')
+    }else if (password !== password2) {
+        return errMessage('密码和确认密码不一致！')
+    }
+
+    // 表单前台验证成功，返回一个发ajax请求的异步action
     return async dispatch => {
         // 不传递password2，后台不需要
         const response = await reqRegister({ username, password, type })
