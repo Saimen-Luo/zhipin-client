7917fbe0386dd6ab68410deb2fbe0088b47a2574
Saimen-Luo <lswofficial@163.com>
2019-05-05 10:20:21 +0800

    finish employee-info static page

--- src/containers/employee-info/employee-info.jsx
+++ src/containers/employee-info/employee-info.jsx
@@ -1,10 +1,20 @@
 /* 大神完善信息容器路由组件 */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile'
+
+import HeaderSelector from '../../components/header-selector/header-selector'
+
 class EmployeeInfo extends Component {
     render() {
         return (
-            <div>EmployeeInfo</div>
+            <div>
+                <NavBar>大神信息完善</NavBar>
+                <HeaderSelector />
+                <InputItem>求职岗位：</InputItem>
+                <TextareaItem title='个人介绍：' rows={3}></TextareaItem>
+                <Button type='primary'>保存</Button>
+            </div>
         )
     }
 }
