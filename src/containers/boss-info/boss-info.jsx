d8abe165a48218399beebc1d3cf7b3039735189a
Saimen-Luo <lswofficial@163.com>
2019-05-05 11:37:32 +0800

    gather boss input info

--- src/containers/boss-info/boss-info.jsx
+++ src/containers/boss-info/boss-info.jsx
@@ -6,16 +6,32 @@ import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile'
 import HeaderSelector from '../../components/header-selector/header-selector'
 
 class BossInfo extends Component {
+    state = {
+        header: '', // 头像名称
+        post: '', // 职位
+        info: '', // 个人或职位简介
+        company: '', // 公司名称
+        salary: '' // 工资
+    }
+    handleChange = (name,val) => {
+        this.setState({
+            [name]:val
+        })
+    }
+    save= ()=> {
+        console.log(this.state);
+        
+    }
     render() {
         return (
             <div>
                 <NavBar>老板信息完善</NavBar>
                 <HeaderSelector />
-                <InputItem>招聘职位：</InputItem>
-                <InputItem>公司名称：</InputItem>
-                <InputItem>职位薪资：</InputItem>
-                <TextareaItem title='职位要求：' rows={3}></TextareaItem>
-                <Button type='primary'>保存</Button>
+                <InputItem onChange={val => { this.handleChange('post', val) }}>招聘职位：</InputItem>
+                <InputItem onChange={val => { this.handleChange('company', val) }}>公司名称：</InputItem>
+                <InputItem onChange={val => { this.handleChange('salary', val) }}>职位薪资：</InputItem>
+                <TextareaItem title='职位要求：' rows={3} onChange={val => { this.handleChange('info', val) }}></TextareaItem>
+                <Button type='primary' onClick={this.save}>保存</Button>
             </div>
         )
     }
