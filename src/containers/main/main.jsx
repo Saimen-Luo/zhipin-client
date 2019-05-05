4498230eb96b5c460a4ee97e7b7094ce879406e7
Saimen-Luo <lswofficial@163.com>
2019-05-06 00:06:17 +0800

    redirect to login page when  not login (no _id)

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -1,12 +1,18 @@
 // main组件
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import BossInfo from '../boss-info/boss-info'
 import EmployeeInfo from '../employee-info/employee-info'
 
-export default class Main extends Component {
+class Main extends Component {
     render() {
+        const { user } = this.props
+        if (!user._id) {
+            // 通过_id判断用户是否登录，无_id则重定向到登录界面；适用于用户未登录通过地址访问其他路由 和 cookies被删除等情况
+            return <Redirect to='login' />
+        }
         return (
             <div>
                 <Switch>
@@ -17,3 +23,7 @@ export default class Main extends Component {
         )
     }
 }
+
+export default connect(
+    state => ({ user: state.user })
+)(Main)
