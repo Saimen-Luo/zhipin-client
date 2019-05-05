07a300263c203c1dfe88bef09886f696646d81d5
Saimen-Luo <lswofficial@163.com>
2019-05-05 13:53:16 +0800

    finish gather employee info

--- src/containers/employee-info/employee-info.jsx
+++ src/containers/employee-info/employee-info.jsx
@@ -6,14 +6,33 @@ import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile'
 import HeaderSelector from '../../components/header-selector/header-selector'
 
 class EmployeeInfo extends Component {
+    state = {
+        header: '', // 头像名称
+        post: '', // 职位
+        info: '', // 个人或职位简介
+    }
+    handleChange = (name, val) => {
+        this.setState({
+            [name]: val
+        })
+    }
+    save = () => {
+        console.log(this.state);
+
+    }
+    setHeader = (header) => {
+        this.setState({
+            header
+        })
+    }
     render() {
         return (
             <div>
                 <NavBar>大神信息完善</NavBar>
-                <HeaderSelector />
-                <InputItem>求职岗位：</InputItem>
-                <TextareaItem title='个人介绍：' rows={3}></TextareaItem>
-                <Button type='primary'>保存</Button>
+                <HeaderSelector setHeader={this.setHeader} />
+                <InputItem onChange={val => { this.handleChange('post', val) }}>求职岗位：</InputItem>
+                <TextareaItem title='个人介绍：' rows={3} onChange={val => { this.handleChange('info', val) }}></TextareaItem>
+                <Button type='primary' onClick={this.save}>保存</Button>
             </div>
         )
     }
