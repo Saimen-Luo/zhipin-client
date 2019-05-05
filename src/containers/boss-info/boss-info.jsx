20a9edfc68ff7dd31debe7d9aadf11cf17ae05da
Saimen-Luo <lswofficial@163.com>
2019-05-05 10:00:45 +0800

    finish boss-info

--- src/containers/boss-info/boss-info.jsx
+++ src/containers/boss-info/boss-info.jsx
@@ -1,10 +1,22 @@
 /* 老板完善信息容器路由组件 */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile'
+
+import HeaderSelector from '../../components/header-selector/header-selector'
+
 class BossInfo extends Component {
     render() {
         return (
-            <div>BossInfo</div>
+            <div>
+                <NavBar>老板信息完善</NavBar>
+                <HeaderSelector />
+                <InputItem>招聘职位：</InputItem>
+                <InputItem>公司名称：</InputItem>
+                <InputItem>职位薪资：</InputItem>
+                <TextareaItem title='职位要求：' rows={3}></TextareaItem>
+                <Button type='primary'>保存</Button>
+            </div>
         )
     }
 }
