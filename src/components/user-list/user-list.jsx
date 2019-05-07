6115d779c9e528d2f113afb0137cdb6c76d7141d
Saimen-Luo <lswofficial@163.com>
2019-05-07 15:34:01 +0800

    map div not WingBlank

--- src/components/user-list/user-list.jsx
+++ src/components/user-list/user-list.jsx
@@ -14,25 +14,27 @@ export default class UserList extends Component {
     render() {
         const { userList } = this.props
         return (
-            userList.map(user => (
-                <WingBlank key={user._id}>
-                    <div>
-                        <WhiteSpace />
-                        <Card>
-                            <Header
-                                thumb={require(`../../assets/images/${user.header}.png`)}
-                                extra={user.username}
-                            />
-                            <Body>
-                                <div>职位: {user.post}</div>
-                                {user.company ? <div>公司: {user.company}</div> : null}
-                                {user.salary ? <div>月薪: {user.salary}</div> : null}
-                                <div>描述: {user.info}</div>
-                            </Body>
-                        </Card>
-                    </div>
-                </WingBlank>
-            ))
+            <WingBlank>
+                {
+                    userList.map(user => (
+                        <div key={user._id}>
+                            <WhiteSpace />
+                            <Card>
+                                <Header
+                                    thumb={require(`../../assets/images/${user.header}.png`)}
+                                    extra={user.username}
+                                />
+                                <Body>
+                                    <div>职位: {user.post}</div>
+                                    {user.company ? <div>公司: {user.company}</div> : null}
+                                    {user.salary ? <div>月薪: {user.salary}</div> : null}
+                                    <div>描述: {user.info}</div>
+                                </Body>
+                            </Card>
+                        </div>
+                    ))
+                }
+            </WingBlank>
         )
     }
 }
