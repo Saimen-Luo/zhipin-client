427943cf5c239952b0a454223df07a7bcacb0d46
Saimen-Luo <lswofficial@163.com>
2019-05-09 13:13:39 +0800

    finish showing single chat page

--- src/containers/chat/chat.jsx
+++ src/containers/chat/chat.jsx
@@ -26,22 +26,48 @@ class Chat extends Component {
 
     }
     render() {
+        const { user } = this.props
+        const { users, chatMsgs } = this.props.chat
+
+        // 计算当前聊天chat_id
+        const meId = user._id
+        if (!users[meId]) { // 如果还没有获取到数据，直接不做任何显示，避免的对话界面刷新报错
+            return null
+        }
+        const targetId = this.props.match.params.userId
+        const chatId = [meId, targetId].sort().join('_')
+
+        // 得到目标用户的icon图标对象
+        const targetHeader = users[targetId].header
+        const targetIcon = targetHeader ? require(`../../assets/images/${targetHeader}.png`) : null
+
+        // 对chatMsgs进行过滤
+        const msgs = chatMsgs.filter(msg => msg.chat_id === chatId)
+
         return (
             <div id='chat-page'>
                 <NavBar>aa</NavBar>
                 <List>
-                    <Item thumb={require('../../assets/images/头像1.png')}>
-                        你好
-                    </Item>
-                    <Item thumb={require('../../assets/images/头像1.png')}>
-                        你好 2
-                    </Item>
-                    <Item className='chat-me' extra=' 我'>
-                        很好
-                    </Item>
-                    <Item className='chat-me' extra=' 我'>
-                        很好 2
-                    </Item>
+                    {
+                        msgs.map(msg => {
+                            if (targetId === msg.from) { // 别人发给我的
+                                return (
+                                    <Item thumb={targetIcon} key={msg._id}>
+                                        {msg.content}
+                                    </Item>
+                                )
+                            } else { // 我发给别人的
+                                return (
+                                    <Item className='chat-me' extra=' 我' key={msg._id}>
+                                        {msg.content}
+                                    </Item>
+                                )
+                            }
+                        })
+                    }
+
+
+
                 </List>
                 <div className='am-tab-bar'>
                     <InputItem value={this.state.content} onChange={(val) => this.setState({ 'content': val })} placeholder=" 请输入" extra={<span onClick={this.handleSend}>发送</span>} />
@@ -52,6 +78,6 @@ class Chat extends Component {
 }
 
 export default connect(
-    state => ({ user: state.user }),
+    state => ({ user: state.user, chat: state.chat }),
     { sendMsg }
 )(Chat)
